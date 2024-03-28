c74f87690bc639392120d4a3f1aa40a56c2f1b90
yaofeihu <yaofeihu@joyy.com>
2024-03-28 11:51:36 +0800

    test-1

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/zuoziye1/go-modules-test-1
+
+go 1.21.1
+
+require github.com/zuoziye1/go-modules-test-2 v1.0.0
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/zuoziye1/go-modules-test-2/utils"
+)
+
+func main() {
+	fmt.Println(utils.TestStr())
+}
